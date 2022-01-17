1facb59cdf5106f34aace6270ec61e641853cd8d
MathisBullinger <mathis@bullinger.dev>
2022-01-17 13:59:11 +0100

    object & light arrangement

--- src/main.js
+++ src/main.js
@@ -5,12 +5,7 @@ const ctx = canvas.getContext('2d')
 canvas.width = window.innerWidth * devicePixelRatio;
 canvas.height = window.innerHeight * devicePixelRatio;
 
-const raysW = canvas.width
-const raysH = Math.ceil(canvas.height / canvas.width * raysW)
-
-async function render() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-
+async function render(raysW, raysH = Math.ceil(canvas.height / canvas.width * raysW)) {
   const pos = [0, 0, 0]
   const vrW = 1
   const vrH = canvas.height / canvas.width * vrW
@@ -22,7 +17,7 @@ async function render() {
         y / raysH * -vrH + vrH / 2,
         1
       ]
-      const cl = rgb(...trace(pos, norm(dir)))
+      const cl = rgb(...trace(pos, norm(dir)) ?? [0,0,0])
       const sx = x * (canvas.width / raysW)
       const sy = y * (canvas.height / raysH)
       ctx.fillStyle = cl
@@ -33,31 +28,52 @@ async function render() {
 }
 
 const objs = [
-  [[0,0,5], 1],
-  [[2,1,8], 1],
+  [[-7, 7, 10], 9]
 ]
 
-const nl = 50
+for (let x = -1; x <= 1; x++)
+  for (let y = -1; y <= 1; y++)
+    for (let z = 0; z < 3; z++)
+      objs.push([[x*3+2,y*3-2,z*3+12], 1])
+
+const nl = 300
+const gr = (1 + 5 ** .5) / 2
 for (let i = 0; i < nl; i++) {
-  const x = Math.sin(i / nl * 2 * Math.PI) * 20
-  const z = Math.cos(i / nl * 2 * Math.PI) * 20
-  const y = Math.random() * 100 - 50
-  const r = 1 + Math.random() * 2
-  const cl = Array(3).fill().map(v => Math.random() * .5 + .5)
-  objs.push([[x, y, z], r, cl])
+  const theta = 2 * Math.PI * i / gr
+  const phi = Math.acos(1 - 2 * (i + 0.5) / nl) 
+  objs.push([
+    [
+      Math.cos(theta) * Math.sin(phi),
+      Math.cos(phi),
+      Math.sin(theta) * Math.sin(phi),
+    ].map(v => v * 100),
+    8,
+    [i / nl * 1, 0, (1 - i / nl) * 1]
+  ])
 }
 
-function trace(pos, dir) {
-  let mags = objs.map((v,i) => [magSq(sub(v, pos)), i]).sort(([a], [b]) => a-b)
+let lm = null
+let getOrd = pos => {
+  if (lm && lm[0][0] === pos[0] && lm[0][1] === pos[1] && lm[0][2] === pos[2]) return lm[1]
+  const mags = objs.map(([v, r],i) => [mag(sub(v, pos))-r, i]).sort(([a], [b]) => a-b).map(([,i]) => i)
+  lm = [pos, mags]
+  return mags
+}
+
+
+function trace(pos, dir, lvl = 0) {
+  if (lvl >= 5) return null
+  const mags = getOrd(pos)
   for (let i = 0; i < objs.length; i++) {
-    let obj = objs[mags[i][1]]
+    let obj = objs[mags[i]]
+    if (lvl === 0 && obj[2]) continue
     const v = intersects(pos, dir, obj[0], obj[1])
     if (!v) continue
     if (obj[2]) return obj[2]
-    const cl = trace(v, reflect(dir, norm(sub(v, obj[0]))))
-    return mult(cl, .8)
+    const cl = trace(v, reflect(dir, norm(sub(v, obj[0]))), lvl+1)
+    return cl ? mult(cl, .8) : null
   }
-  return [0,0,0] 
+  return null 
 }
 
 function intersects(o, u, c, r) {
@@ -72,3 +88,10 @@ function intersects(o, u, c, r) {
   
 
 render()
+
+;(async () => {
+  const r = canvas.width
+  await render(r / 16)
+  await render(r / 4)
+  await render(r)
+})()
