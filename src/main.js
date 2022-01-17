cdd7ad81dc9cf17a2340976527d9909bce2863ab
Mathis Bullinger <mathis@bullinger.dev>
2022-01-17 15:36:47 +0100

    light falloff

--- src/main.js
+++ src/main.js
@@ -1,97 +1,109 @@
-import { mag, magSq, dot, add, sub, mult, norm, rgb, reflect } from './vector'
+import { mag, dot, add, sub, mult, norm, rgb, reflect } from './vector'
 
+const renderLights = true
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
-canvas.width = window.innerWidth * devicePixelRatio;
-canvas.height = window.innerHeight * devicePixelRatio;
+canvas.width = window.innerWidth * devicePixelRatio
+canvas.height = window.innerHeight * devicePixelRatio
 
-async function render(raysW, raysH = Math.ceil(canvas.height / canvas.width * raysW)) {
+const clAss = (ch, d) => {
+  let max = 0
+  const adj = ch.map((v) => {
+    const r = v * (1.2e4 / d ** 2)
+    if (r > max) max = r
+    return r
+  })
+  return rgb(...(max <= 1 ? adj : adj.map((v) => v * (1 / max))))
+}
+
+async function render(
+  raysW,
+  raysH = Math.ceil((canvas.height / canvas.width) * raysW)
+) {
   const pos = [0, 0, 0]
   const vrW = 1
-  const vrH = canvas.height / canvas.width * vrW
-  
+  const vrH = (canvas.height / canvas.width) * vrW
+
   for (let y = 0; y < raysH; y++) {
     for (let x = 0; x < raysW; x++) {
-      const dir = [
-        x / raysW * vrW - vrW / 2,
-        y / raysH * -vrH + vrH / 2,
-        1
-      ]
-      const cl = rgb(...trace(pos, norm(dir)) ?? [0,0,0])
+      const dir = [(x / raysW) * vrW - vrW / 2, (y / raysH) * -vrH + vrH / 2, 1]
+      const r = trace(pos, norm(dir))
+      const cl = r ? clAss(r[0], r[1]) : '#000'
       const sx = x * (canvas.width / raysW)
       const sy = y * (canvas.height / raysH)
       ctx.fillStyle = cl
       ctx.fillRect(sx, sy, canvas.width / raysW, canvas.height / raysH)
-      if ((y * raysW + x) % (4 * raysW) === 0) await new Promise(requestAnimationFrame)
+      if ((y * raysW + x) % (4 * raysW) === 0)
+        await new Promise(requestAnimationFrame)
     }
   }
 }
 
-const objs = [
-  [[-7, 7, 10], 9]
-]
+const objs = [[[-7, 7, 10], 9]]
 
 for (let x = -1; x <= 1; x++)
   for (let y = -1; y <= 1; y++)
     for (let z = 0; z < 3; z++)
-      objs.push([[x*3+2,y*3-2,z*3+12], 1])
+      objs.push([[x * 3 + 2, y * 3 - 2, z * 3 + 12], 1])
 
 const nl = 300
-const gr = (1 + 5 ** .5) / 2
+const gr = (1 + 5 ** 0.5) / 2
 for (let i = 0; i < nl; i++) {
-  const theta = 2 * Math.PI * i / gr
-  const phi = Math.acos(1 - 2 * (i + 0.5) / nl) 
+  const theta = (2 * Math.PI * i) / gr
+  const phi = Math.acos(1 - (2 * (i + 0.5)) / nl)
   objs.push([
     [
       Math.cos(theta) * Math.sin(phi),
       Math.cos(phi),
       Math.sin(theta) * Math.sin(phi),
-    ].map(v => v * 100),
+    ].map((v) => v * 100),
     8,
-    [i / nl * 1, 0, (1 - i / nl) * 1]
+    [(i / nl) * 1, 0, (1 - i / nl) * 1],
   ])
 }
 
 let lm = null
-let getOrd = pos => {
-  if (lm && lm[0][0] === pos[0] && lm[0][1] === pos[1] && lm[0][2] === pos[2]) return lm[1]
-  const mags = objs.map(([v, r],i) => [mag(sub(v, pos))-r, i]).sort(([a], [b]) => a-b).map(([,i]) => i)
+let getOrd = (pos) => {
+  if (lm && lm[0][0] === pos[0] && lm[0][1] === pos[1] && lm[0][2] === pos[2])
+    return lm[1]
+  const mags = objs
+    .map(([v, r], i) => [mag(sub(v, pos)) - r, i])
+    .sort(([a], [b]) => a - b)
+    .map(([, i]) => i)
   lm = [pos, mags]
   return mags
 }
-
 
 function trace(pos, dir, lvl = 0) {
   if (lvl >= 5) return null
   const mags = getOrd(pos)
   for (let i = 0; i < objs.length; i++) {
     let obj = objs[mags[i]]
-    if (lvl === 0 && obj[2]) continue
-    const v = intersects(pos, dir, obj[0], obj[1])
+    if (lvl === 0 && !renderLights && obj[2]) continue
+    const v = intersect(pos, dir, obj[0], obj[1])
     if (!v) continue
-    if (obj[2]) return obj[2]
-    const cl = trace(v, reflect(dir, norm(sub(v, obj[0]))), lvl+1)
-    return cl ? mult(cl, .8) : null
+    const cr = obj[2]
+      ? [obj[2], 0]
+      : trace(v[0], reflect(dir, norm(sub(v[0], obj[0]))), lvl + 1)
+    if (!cr) return null
+    return [cr[0], cr[1] + v[1]]
   }
-  return null 
+  return null
 }
 
-function intersects(o, u, c, r) {
+function intersect(o, u, c, r) {
   const omc = sub(o, c)
   const du = dot(u, omc)
   const s = du ** 2 - (mag(omc) ** 2 - r ** 2)
-  if (s <= 0) return null 
+  if (s <= 0) return null
   const d = -du - Math.sqrt(s)
   if (d <= 0) return null
-  return add(o, mult(u, d))
-}  
-  
+  return [add(o, mult(u, d)), d]
+}
 
 render()
-
 ;(async () => {
   const r = canvas.width
   await render(r / 16)
-  await render(r / 4)
-  await render(r)
+  await render(r / 2)
 })()
